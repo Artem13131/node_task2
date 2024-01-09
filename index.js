511116b5fd66b228987977b0fd72288bda5cd660
Tema <veselow.artemka@yandex.ru>
2024-01-09 13:43:31 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express');
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
-const app = express();
 
-app.get('/express', function(req, res){
-	res.send("<h2>Привет Express!</h2>");
-});
+app.use(bodyParser.urlencoded({ extended: true })); //Для принятия данных из формы. Обязательно!!!
+app.use(bodyParser.json());//Для принятия данных из формы. Обязательно!!!
 
-app.listen(3000, "localhost");
+
+let index = require('./routes/index');
+app.use('/', index);
+
+
+let article = require('./routes/article');
+app.use('/article', article);
+
+
+let admin = require('./routes/admin');
+app.use('/admin', admin);
+
+
+app.listen(3000, 'localhost');
