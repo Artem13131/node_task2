cae73b45a6ab0da69fcb47ddfcbf7e1392b2afa4
Tema <veselow.artemka@yandex.ru>
2024-02-08 19:37:29 +0300

    changes in files

--- mysql.js
+++ mysql.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    database: "article",
+    user: "sqlite3",
+    database: "library.db",
     password: ""
   }).promise();
   connection.connect(function(err){
